70fb2b252bf052f9051d211593c2e7b522116413
Marek Zelinka <mzelinka17@gmail.com>
2023-02-14 16:38:27 +0100

    extract button

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,19 +1,10 @@
 import { useState } from "react";
+import { Button } from "./button";
 
 function Display(props) {
   const { count } = props;
 
   return <div>{count}</div>;
-}
-
-function Button(props) {
-  const { children, type = "button", ...restProps } = props;
-
-  return (
-    <button type={type} {...restProps}>
-      {children}
-    </button>
-  );
 }
 
 export function Counter() {
