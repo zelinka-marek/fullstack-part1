3d3daf29f8fb84bb2a160fee526f0253c41062d7
Marek Zelinka <mzelinka17@gmail.com>
2023-02-14 12:35:07 +0100

    intro to components

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,20 @@
+const dateFormatter = new Intl.DateTimeFormat("en", {
+  timeStyle: "short",
+  dateStyle: "long",
+});
+
 export function App() {
-  return <div />;
+  const now = new Date();
+  const a = 10;
+  const b = 20;
+  console.log(now, a + b);
+
+  return (
+    <div>
+      <p>Hello world, it is {dateFormatter.format(now)}</p>
+      <p>
+        {a} plus {b} is {a + b}
+      </p>
+    </div>
+  );
 }
